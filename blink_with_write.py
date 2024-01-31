7c68f19524bfd4abdead49e23514bc8fcbf3ff80
yuzihaowashu <yu.zihao@wustl.edu>
2024-01-30 20:41:12 -0600

    Check indent

--- blink_with_write.py
+++ blink_with_write.py
@@ -9,19 +9,20 @@ start_time = time.time()
 DEBUG = False
 
 if '-debug' in sys.argv:
-    DEBUG = True
+   DEBUG = True
 
 blink_rate = input("Please enter the blink rate: ")
 try:
-    blink_rate = float(blink_rate)
+   blink_rate = float(blink_rate)
 except ValueError:
-    blink_rate = 1
+   blink_rate = 1
 
 duration = input("Please enter the total running time of program: ")
+
 try:
-    duration = float(duration)
+   duration = float(duration)
 except ValueError:
-    duration = 15 #Default time is 15 seconds
+   duration = 15 #Default time is 15 seconds
 
 LED_PIN = 11
 LED_IS_ON = False
@@ -37,26 +38,26 @@ GPIO.setup(SWITCH_PIN, GPIO.IN)
 
 with open('data.txt', 'w') as data:
 
-    while True:
-        # Stop the program when time ends
-        stop_time = time.time() - start_time
-        if stop_time >= duration:
-            break
+   while True:
+      # Stop the program when time ends
+      stop_time = time.time() - start_time
+      if stop_time >= duration:
+          break
     
-        # When switch is on, the LED light alternates.
-        if(GPIO.input(SWITCH_PIN)):
+      # When switch is on, the LED light alternates.
+      if(GPIO.input(SWITCH_PIN)):
 
-            GPIO.output(LED_PIN, LED_IS_ON)
-            data.write(f'{time.time():1.0f}     {LED_IS_ON}\n')
-            if DEBUG:
-                print(f'LED is on: {LED_IS_ON}\n')
-            LED_IS_ON = not(LED_IS_ON)
-            time.sleep(blink_rate)
+         GPIO.output(LED_PIN, LED_IS_ON)
+         data.write(f'{time.time():1.0f}     {LED_IS_ON}\n')
+         if DEBUG:
+            print(f'LED is on: {LED_IS_ON}\n')
+         LED_IS_ON = not(LED_IS_ON)
+         time.sleep(blink_rate)
 
-       #When switch is off, the LED is always OFF.
-       else:
-            if DEBUG:
-                print("The switch is OFF.")
+      #When switch is off, the LED is always OFF.
+      else:
+         if DEBUG:
+            print("The switch is OFF.")
             LED_IS_ON = False
             GPIO.output(LED_PIN, LED_IS_ON)
             data.write(f'{time.time():1.0f}     {LED_IS_ON}\n')
